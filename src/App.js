3555e421988f18c197d257e624cc14a24b8f8e2f
Kevin Chen <kevin.chen.krc@gmail.com>
2020-06-08 19:39:11 -0400

    Add controlled input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import CardEditor from "./CardEditor";
 import "./App.css";
 
-function App() {
-  return <div className="App">Testing</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { front: "front1", back: "back1" },
+        { front: "front2", back: "back2" },
+      ],
+    };
+  }
+
+  render() {
+    return <CardEditor cards={this.state.cards} />;
+  }
 }
 
 export default App;
